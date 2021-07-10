efd2a16f7bee75b4efa7b133ee79657467f4f669
Victor Enrique <victorenriquecandidodossantos@gmail.com>
2021-07-09 23:11:14 -0400

    1 - Criado formulario adicionar produto.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  const [name, setName] = useState("react");
+  const [descricao, setDescricao] = useState("react");
+  const [preco, setPreco] = useState(0);
+
+  const displayInfo = () =>{
+    console.log(name, descricao, preco);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="information">
+    <label>Nome:</label>
+    <input type="text" onChange={(event) => {
+      setName(event.target.value);
+    }} ></input>
+      <label>Descrição:</label>
+      <input type="text" onChange={(event) => {
+      setDescricao(event.target.value);
+    }} ></input>
+      <label>Preço:</label>
+      <input type="text" onChange={(event) => {
+      setPreco(event.target.value);
+    }} ></input>
+      <button onClick={displayInfo}>Adicionar produto</button>
+    </div>
     </div>
   );
 }
