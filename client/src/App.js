42e3490aa8fd6ae61f2964a802b21194bc91b2a2
Victor Enrique <victorenriquecandidodossantos@gmail.com>
2021-07-10 02:21:59 -0400

    1 - Arrumado linhas no front. 2 - API restantes adicionada (CRUD).

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
+import "./App.css";
 import { useState } from "react";
 import Axios from "axios";
 
 function App() {
-
-  const [nome, setNome] = useState("react");
-  const [descricao, setDescricao] = useState("react");
+  const [nome, setNome] = useState("");
+  const [descricao, setDescricao] = useState("");
   const [preco, setPreco] = useState(0);
+  const [newPreco, setNewPreco] = useState(0);
+  const [produtoLista, setProdutoLista] = useState([]);
 
   const addProduto = () => {
     Axios.post("http://localhost:3001/create", {
@@ -14,27 +15,119 @@ function App() {
       descricao: descricao,
       preco: preco,
     }).then(() => {
-      console.log("sucesso");
+      setProdutoLista([
+        ...produtoLista,
+        {
+          nome: nome,
+          descricao: descricao,
+          preco: preco,
+        },
+      ]);
+    });
+  };
+
+  const getProdutos = () => {
+    Axios.get("http://localhost:3001/produtos").then((response) => {
+      setProdutoLista(response.data);
+    });
+  };
+
+  const updateProduto = (id) => {
+    Axios.put("http://localhost:3001/update", { preco: newPreco, id: id }).then(
+      (response) => {
+        setProdutoLista(
+          produtoLista.map((val) => {
+            return val.id == id
+              ? {
+                  id: val.id,
+                  nome: val.nome,
+                  descricao: val.descricao,
+                  preco: newPreco,
+                }
+              : val;
+          })
+        );
+      }
+    );
+  };
+
+  const deleteProduto = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
+      setProdutoLista(
+        produtoLista.filter((val) => {
+          return val.id != id;
+        })
+      );
     });
   };
 
   return (
     <div className="App">
+      <h1>Desafio ICTS</h1>
       <div className="information">
-    <label>Nome:</label>
-    <input type="text" onChange={(event) => {
-      setNome(event.target.value);
-    }} ></input>
-      <label>Descrição:</label>
-      <input type="text" onChange={(event) => {
-      setDescricao(event.target.value);
-    }} ></input>
-      <label>Preço:</label>
-      <input type="text" onChange={(event) => {
-      setPreco(event.target.value);
-    }} ></input>
-      <button onClick={addProduto}>Adicionar produto</button>
-    </div>
+        <label>Nome do produto:</label>
+        <input
+          type="text" placeholder="Exemplo: Playstation 4"
+          onChange={(event) => {
+            setNome(event.target.value);
+          }}
+        />
+        <label>Descrição:</label>
+        <input
+          type="text" placeholder="Exemplo: Sony Playstation 4 Slim"
+          onChange={(event) => {
+            setDescricao(event.target.value);
+          }}
+        />
+        <label>Preço:</label>
+        <input
+          type="number" placeholder="Exemplo: 4,000"
+          onChange={(event) => {
+            setPreco(event.target.value);
+          }}
+        />
+        <button onClick={addProduto}>Adicionar Produtos</button>
+      </div>
+      <div className="produtos">
+        <button onClick={getProdutos}>Mostrar Produtos</button>
+
+        {produtoLista.map((val, key) => {
+          return (
+            <div className="produtos">
+              <div>
+                <h3>Nome: {val.nome}</h3>
+                <h3>Descrição: {val.descricao}</h3>
+                <h3>Preço: {val.preco}</h3>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="Digite o novo preço do produto..."
+                  onChange={(event) => {
+                    setNewPreco(event.target.value);
+                  }}
+                />
+                <button
+                  onClick={() => {
+                    updateProduto(val.id);
+                  }}
+                >
+                  {" "}
+                  Atualizar Preço
+                </button>
+
+                <button
+                  onClick={() => {
+                    deleteProduto(val.id);
+                  }}
+                >
+                  Deletar Produto
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
     database: 'desafioICTS',
 });
 
-app.post('/create', (req, res) => {
+app.post("/create", (req, res) => {
     const nome = req.body.nome;
     const descricao = req.body.descricao;
     const preco = req.body.preco;
@@ -29,6 +29,43 @@ app.post('/create', (req, res) => {
     });
 });
 
-app.listen(3001, () =>{
-    console.log("Seu servidor está rodando na porta 3001")
+app.get("/produtos", (req, res) => {
+  db.query("SELECT * FROM produtos", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.put("/update", (req, res) => {
+  const id = req.body.id;
+  const preco = req.body.preco;
+  db.query(
+    "UPDATE produtos SET preco = ? WHERE id = ?",
+    [preco, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM produtos WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Yey, your server is running on port 3001");
 });
